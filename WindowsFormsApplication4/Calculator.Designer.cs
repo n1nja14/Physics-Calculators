ad7e1c96b2f88a8029d90655669dbb1c19e1b8d8
yo <n1nja147@yandex.ru>
2023-02-27 21:58:15 +0500

    update

--- WindowsFormsApplication4/Calculator.Designer.cs
+++ WindowsFormsApplication4/Calculator.Designer.cs
@@ -30,24 +30,17 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            this.radioButton5 = new System.Windows.Forms.RadioButton();
-            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(280, 296);
+            this.textBox1.Location = new System.Drawing.Point(285, 347);
             this.textBox1.Margin = new System.Windows.Forms.Padding(6);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(196, 37);
@@ -57,29 +50,18 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox2.Location = new System.Drawing.Point(280, 408);
+            this.textBox2.Location = new System.Drawing.Point(722, 347);
             this.textBox2.Margin = new System.Windows.Forms.Padding(6);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(196, 37);
             this.textBox2.TabIndex = 2;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Enabled = false;
-            this.textBox3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox3.Location = new System.Drawing.Point(240, 533);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(6);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(278, 37);
-            this.textBox3.TabIndex = 3;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.Control;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(296, 242);
+            this.label1.Location = new System.Drawing.Point(301, 293);
             this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(166, 29);
@@ -92,24 +74,12 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.Control;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(298, 364);
+            this.label2.Location = new System.Drawing.Point(740, 303);
             this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(164, 29);
             this.label2.TabIndex = 5;
             this.label2.Text = "Второе число";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.SystemColors.Control;
-            this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(322, 492);
-            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(118, 29);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Результат";
             // 
             // button1
             // 
@@ -148,93 +118,13 @@
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // radioButton1
+            // richTextBox1
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.BackColor = System.Drawing.SystemColors.Control;
-            this.radioButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.radioButton1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.radioButton1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.radioButton1.Location = new System.Drawing.Point(692, 242);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(6);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(156, 33);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.Text = "Сложение";
-            this.radioButton1.UseVisualStyleBackColor = false;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.BackColor = System.Drawing.SystemColors.Control;
-            this.radioButton2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.radioButton2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.radioButton2.Location = new System.Drawing.Point(692, 301);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(6);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(168, 33);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.Text = "Вычитание";
-            this.radioButton2.UseVisualStyleBackColor = false;
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.BackColor = System.Drawing.SystemColors.Control;
-            this.radioButton3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.radioButton3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.radioButton3.Location = new System.Drawing.Point(692, 360);
-            this.radioButton3.Margin = new System.Windows.Forms.Padding(6);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(172, 33);
-            this.radioButton3.TabIndex = 2;
-            this.radioButton3.Text = "Умножение";
-            this.radioButton3.UseVisualStyleBackColor = false;
-            // 
-            // radioButton4
-            // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.BackColor = System.Drawing.SystemColors.Control;
-            this.radioButton4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.radioButton4.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.radioButton4.Location = new System.Drawing.Point(692, 419);
-            this.radioButton4.Margin = new System.Windows.Forms.Padding(6);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(139, 33);
-            this.radioButton4.TabIndex = 3;
-            this.radioButton4.Text = "Деление";
-            this.radioButton4.UseVisualStyleBackColor = false;
-            // 
-            // radioButton5
-            // 
-            this.radioButton5.AutoSize = true;
-            this.radioButton5.BackColor = System.Drawing.SystemColors.Control;
-            this.radioButton5.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.radioButton5.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.radioButton5.Location = new System.Drawing.Point(692, 478);
-            this.radioButton5.Margin = new System.Windows.Forms.Padding(6);
-            this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(261, 33);
-            this.radioButton5.TabIndex = 4;
-            this.radioButton5.Text = "Корень квадратный";
-            this.radioButton5.UseVisualStyleBackColor = false;
-            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
-            // 
-            // radioButton6
-            // 
-            this.radioButton6.AutoSize = true;
-            this.radioButton6.BackColor = System.Drawing.SystemColors.Control;
-            this.radioButton6.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.radioButton6.FlatAppearance.BorderSize = 0;
-            this.radioButton6.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.radioButton6.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.radioButton6.Location = new System.Drawing.Point(692, 537);
-            this.radioButton6.Margin = new System.Windows.Forms.Padding(6);
-            this.radioButton6.Name = "radioButton6";
-            this.radioButton6.Size = new System.Drawing.Size(136, 33);
-            this.radioButton6.TabIndex = 5;
-            this.radioButton6.Text = "Степень";
-            this.radioButton6.UseVisualStyleBackColor = false;
+            this.richTextBox1.Location = new System.Drawing.Point(68, 34);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(941, 206);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
             // 
             // Calculator
             // 
@@ -243,22 +133,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.BackgroundImage = global::WindowsFormsApplication4.Properties.Resources.калькулятор_по_физ1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.CancelButton = this.button2;
             this.ClientSize = new System.Drawing.Size(1202, 962);
-            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.radioButton5);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.radioButton4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Margin = new System.Windows.Forms.Padding(6);
@@ -279,17 +161,10 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton4;
-        private System.Windows.Forms.RadioButton radioButton5;
-        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
--- WindowsFormsApplication4/Calculator.cs
+++ WindowsFormsApplication4/Calculator.cs
@@ -58,84 +58,186 @@ namespace WindowsFormsApplication4
         {
             
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        public string Create_OPZ(string input_string)
         {
-            Double  first, second, result;
-            if (radioButton1.Checked)
+            string[] output_string = new string[0];//выходная строка(массив)(так она называется на википедии)
+            string[] stack = new string[0];//стек
+
+            int k = 0;
+            for (int i = 0; i < input_string.Length; i++)
             {
-                first = Double.Parse(textBox1.Text);
-                second = Double.Parse(textBox2.Text);
-                result = first + second;
-                textBox3.Text = Convert.ToString(result);
-            }
-            else if (radioButton2.Checked)
-            {
-                first = Double.Parse(textBox1.Text);
-                second = Double.Parse(textBox2.Text);
-                result = first - second;
-                textBox3.Text = Convert.ToString(result);
-            }
-            else if (radioButton3.Checked)
-            {
-                first = Double.Parse(textBox1.Text);
-                second = Double.Parse(textBox2.Text);
-                result = first * second;
-                textBox3.Text = Convert.ToString(result);
-            }
-            else if (radioButton4.Checked)
-            {
-                second = Double.Parse(textBox2.Text);
-                if (second == 0)
+                string temp = "";//как бы это было не удивительно, но переменная для хранения числа
+                //проверка на отрицательное число: зсли знак "-" в начале строки или перед знаком "-" нет числа
+                if (input_string[i] == '-' && ((i > 0 && !Char.IsDigit(input_string[i - 1])) || i == 0))
                 {
-                    MessageBox.Show(
-                    "Деление на ноль невозможно!",
-                    "Внимание!",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Error,
-                    MessageBoxDefaultButton.Button1,
-                    MessageBoxOptions.DefaultDesktopOnly);
+                    i++;
+                    temp += "-";//в переменную для чисел добавляется знак "-"
                 }
+                if (Char.IsDigit(input_string[i]))
+                {
+                    //найдено число и в переменную temp записываются все цифры числа, тк число может состоять не только из одной цифры
+                    while (i < input_string.Length && Char.IsDigit(input_string[i]))
+                        temp += input_string[i++].ToString();
+                    i--;
+                    Array.Resize(ref output_string, output_string.Length + 1);//массив выходной строки динамически увеличиваем на единицу
+                    output_string[output_string.Length - 1] = temp;//добавляем число в текущий элемент выходной строки
+                }
+                if (input_string[i] == '+' || input_string[i] == '-')
+                {
+                m://т.к + и - имеют самый низкий приоритет, то при записывании этих операндов в стек 
+                    //все остальные операции из стека должны переместится в выходную строку(исключение скобка) 
+                    if (stack.Length != 0) //если стек еще не пуст
+                    {
+                        if (stack[stack.Length - 1] == "(")//если последний элемент стека открывающаяся скобка 
+                        {
+                            Array.Resize(ref stack, stack.Length + 1);
+                            stack[stack.Length - 1] = input_string[i].ToString();//то в стек добавляется операция сложения или отрицания 
+                        }
+                        else //иначе
+                        {
+                            Array.Resize(ref output_string, output_string.Length + 1);
+                            output_string[output_string.Length - 1] = stack[stack.Length - 1];//в выходную строку добавляется последняя операция из стека
+                            Array.Resize(ref stack, stack.Length - 1);//у стека удаляется последний элемент
+                            goto m;//возврат на проверку находится ли на вершине стека еще какая либо операция
+                        }
+                        stack[stack.Length - 1] = input_string[i].ToString();//на вершину стека помещается операция + или -
+                    }
+                    else
+                    {
+                        Array.Resize(ref stack, stack.Length + 1);
+                        stack[stack.Length - 1] = input_string[i].ToString();//если стек не пуст то в стек добавляется операция сложения или отрицания
+                    }
+                }
+                if (input_string[i] == '*' || input_string[i] == '/')
+                {
+
+                    if (stack.Length != 0)//если стек еще не пуст
+                    {
+
+                        if (stack[stack.Length - 1] != "*" && stack[stack.Length - 1] != "/")//если на вершине стека не располагается * или /
+                        {
+                            Array.Resize(ref stack, stack.Length + 1);
+                            stack[stack.Length - 1] = input_string[i].ToString();
+                        }
+                        else  //если на вершине стека располагается * или /
+                        {//то т.к приоритеты операций одинаковые... 
+                            Array.Resize(ref output_string, output_string.Length + 1);
+                            output_string[output_string.Length - 1] = stack[stack.Length - 1];//...элемент с вершины стека передается в выходную строку(массив)
+                            stack[stack.Length - 1] = input_string[i].ToString();//на вершину стека становится текщая операция умножения или деления
+
+                        }
+
+                    }
+                    else
+                    {
+                        Array.Resize(ref stack, stack.Length + 1);
+                        stack[stack.Length - 1] = input_string[i].ToString();//если стек не пуст то в стек добавляется операция умножения или деления
+                    }
+                }
+                if (input_string[i] == '(')
+                {//найдена открывающаяся скобка которая автоматически добавляется в стек
+                    Array.Resize(ref stack, stack.Length + 1);
+                    stack[stack.Length - 1] = input_string[i].ToString();
+                }
+                if (input_string[i] == ')')
+                {//найдена закрывающаяся скобка
+                    int k_t = stack.Length - 1;
+                    while (stack[k_t] != "(")//некий цикл который начиная с конца стека перемещается влево в поисках открывающиейся скобки
+                        k_t--;//после цикла здесь хранится индекс открывающейся скобки
+                    int kol = 0;
+                    for (int j = stack.Length - 1; j >= k_t + 1; j--)//все операции которые хранились в стеке после "(" переносятся в выходную строку
+                    {
+                        kol++;
+                        Array.Resize(ref output_string, output_string.Length + 1);
+                        output_string[output_string.Length - 1] = stack[j];
+                    }
+                    Array.Resize(ref stack, stack.Length - (kol + 1));//стек уменьшается на количество переданных операций
+
+                }
+                if (input_string[i] == '^')//т.к у возведения в степень самый большой приоритет, его автоматически добавляют в выходную строку
+                {
+                    Array.Resize(ref stack, stack.Length + 1);
+                    stack[stack.Length - 1] = input_string[i].ToString();
+                }
+            }
+            //всю ВХодную строку проверили теперь в стеке и в ВЫХодной строке содержатся некоторые данные
+            //объединяем выходную строку со стеком
+            Array.Resize(ref output_string, output_string.Length + stack.Length);
+            Array.Reverse(stack);//это просто что бы не парится и не создавать цкл который будет идти задом на перед
+            //т.к иначе есть свои нюансы в которые я вдаваться не хочу
+            for (int i = output_string.Length - stack.Length; i < output_string.Length; i++)
+                output_string[i] = stack[k++];
+            string t = "";//сформировали из выходной строки(массива) строку
+            for (int i = 0; i < output_string.Length; i++)
+                if (i != output_string.Length - 1)
+                    t += output_string[i] + " ";
                 else
-                {
-                    first = Double.Parse(textBox1.Text);
-                    second = Double.Parse(textBox2.Text);
-                    result = first / second;
-                    textBox3.Text = Convert.ToString(result);
-                }
-            }
-            else if (radioButton5.Checked)
-            {
-                first = Double.Parse(textBox1.Text);
-                if (first < 0)
-                {
-                    MessageBox.Show(
-                    "Подкоренное выражение должно быть больше, либо равно 0!",
-                    "Внимание!",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Error,
-                    MessageBoxDefaultButton.Button1,
-                    MessageBoxOptions.DefaultDesktopOnly);
-                }
-                else
-                {
-                    result = Math.Sqrt(first);
-                    textBox3.Text = Convert.ToString(result);
-                }
-            }
-            else if (radioButton6.Checked)
-            {
-                first = Double.Parse(textBox1.Text);
-                second = Double.Parse(textBox2.Text);
-                result = Math.Pow(first, second);
-                textBox3.Text = Convert.ToString(result);
-            }
-            else
-            {
-                
-            }
+                    t += output_string[i];
+            return t;//передали полученную строку
         }
 
+        public double Solution(string OPZ_String)
+        {
+            string[] mas = OPZ_String.Split(' ');//нарезаем строку в виде Обратной польской записи в массив элементов
+            string temp_string;
+            for (int i = 0; i < mas.Length; i++)
+                switch (mas[i])
+                {
+                    case "+"://если найдена операция сложения
+                        temp_string = (double.Parse(mas[i - 2]) + double.Parse(mas[i - 1])).ToString();//выполняем сложение и переводим ее в строку
+                        richTextBox1.AppendText(mas[i - 2] + "+" + mas[i - 1] + "=" + temp_string + "\n");
+                        mas[i - 2] = temp_string;//на место 1-ого операнда записывается результат (как если бы a=a+b)
+                        for (int j = i - 1; j < mas.Length - 2; j++)//удаляем из массива второй операнд и знак арифм действия
+                            mas[j] = mas[j + 2];
+                        Array.Resize(ref mas, mas.Length - 2);//обрезаем массив элементов на 2 удаленнх элемента
+                        i -= 2;
+                        break;
+                    case "-"://далее все аналогично
+                        temp_string = (double.Parse(mas[i - 2]) - double.Parse(mas[i - 1])).ToString();
+                        richTextBox1.AppendText(mas[i - 2] + "-" + mas[i - 1] + "=" + temp_string + "\n");
+                        mas[i - 2] = temp_string;
+                        for (int j = i - 1; j < mas.Length - 2; j++)
+                            mas[j] = mas[j + 2];
+                        Array.Resize(ref mas, mas.Length - 2);
+                        i -= 2;
+                        break;
+                    case "*":
+                        temp_string = (double.Parse(mas[i - 2]) * double.Parse(mas[i - 1])).ToString();
+                        richTextBox1.AppendText(mas[i - 2] + "*" + mas[i - 1] + "=" + temp_string + "\n");
+                        mas[i - 2] = temp_string;
+                        for (int j = i - 1; j < mas.Length - 2; j++)
+                            mas[j] = mas[j + 2];
+                        Array.Resize(ref mas, mas.Length - 2);
+
+                        i -= 2;
+                        break;
+                    case "/":
+                        temp_string = (double.Parse(mas[i - 2]) / double.Parse(mas[i - 1])).ToString();
+                        richTextBox1.AppendText(mas[i - 2] + "/" + mas[i - 1] + "=" + temp_string + "\n");
+                        mas[i - 2] = temp_string;
+                        for (int j = i - 1; j < mas.Length - 2; j++)
+                            mas[j] = mas[j + 2];
+                        Array.Resize(ref mas, mas.Length - 2);
+                        i -= 2;
+                        break;
+                    case "^":
+                        temp_string = (Math.Pow(double.Parse(mas[i - 2]), double.Parse(mas[i - 1]))).ToString();
+                        richTextBox1.AppendText(mas[i - 2] + "^" + mas[i - 1] + "=" + temp_string + "\n");
+                        mas[i - 2] = temp_string;
+                        for (int j = i - 1; j < mas.Length - 2; j++)
+                            mas[j] = mas[j + 2];
+                        Array.Resize(ref mas, mas.Length - 2);
+                        i -= 2;
+                        break;
+                }
+            return double.Parse(mas[0]);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double result = Solution(Create_OPZ(textBox1.Text));
+            textBox2.Text = result.ToString();
+
+        }
         
 
         private void radioButton5_CheckedChanged(object sender, EventArgs e)
