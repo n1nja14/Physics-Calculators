1d81b319ddfb31b8b80db5b8699dc416cf5c7661
Baron138 <114290324+Baron138@users.noreply.github.com>
2023-03-01 16:49:22 +0300

    ТЕОРИЯ

--- WindowsFormsApplication4/PowerArhimedTheory.Designer.cs
+++ WindowsFormsApplication4/PowerArhimedTheory.Designer.cs
@@ -52,12 +52,13 @@
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(137, 112);
+            this.pictureBox1.Location = new System.Drawing.Point(-17, -61);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(294, 208);
+            this.pictureBox1.Size = new System.Drawing.Size(610, 410);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.UseWaitCursor = true;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // PowerArhimedTheory
             // 
--- WindowsFormsApplication4/PowerArhimedTheory.cs
+++ WindowsFormsApplication4/PowerArhimedTheory.cs
@@ -37,5 +37,10 @@ namespace WindowsFormsApplication4
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
